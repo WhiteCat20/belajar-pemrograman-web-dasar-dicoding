b0183c0c01bb7fce286630d13ea283ecac57f2bf
WhiteCat20 <fzrahmadan@gmai;.com>
2022-08-21 19:23:31 +0700

    change the style and content for renewal certificate dicoding

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,37 @@
-const buttonBlender = document.getElementById('blender');
-const buttonHairdryer = document.getElementById('hairdryer');
-const buttonPencukur = document.getElementById('pencukur');
+const arabasta = document.getElementById("arabasta");
+const skypea = document.getElementById("skypea");
+const eniesLobby = document.getElementById("enies-lobby");
 //punya blender
-const modalBg = document.querySelector('.modalbg')
-const modalClose = document.querySelector('.close-modal')
+const modalBg = document.querySelector(".modalbg");
+const modalClose = document.querySelector(".close-modal");
 
+arabasta.addEventListener("click", () => {
+  modalBg.classList.add("activate-bg");
+});
 
-buttonBlender.addEventListener('mouseover', () => {
-    modalBg.classList.add('activate-bg')
-})
-
-modalClose.addEventListener('mouseover', () => {
-    modalBg.classList.remove('activate-bg')
-})
+modalClose.addEventListener("click", () => {
+  modalBg.classList.remove("activate-bg");
+});
 
 //punya hairdryer
-const modalBg1 = document.querySelector('.modalbg1')
-const modalClose1 = document.querySelector('.close-modal1')
+const modalBg1 = document.querySelector(".modalbg1");
+const modalClose1 = document.querySelector(".close-modal1");
 
-buttonHairdryer.addEventListener('mouseover', () => {
-    modalBg1.classList.add('activate-bg1')
-})
+skypea.addEventListener("click", () => {
+  modalBg1.classList.add("activate-bg1");
+});
 
-modalClose1.addEventListener('mouseover', () => {
-    modalBg1.classList.remove('activate-bg1')
-})
+modalClose1.addEventListener("click", () => {
+  modalBg1.classList.remove("activate-bg1");
+});
 
 //punya pencukur
-const modalBg2 = document.querySelector('.modalbg2')
-const modalClose2 = document.querySelector('.close-modal2')
+const modalBg2 = document.querySelector(".modalbg2");
+const modalClose2 = document.querySelector(".close-modal2");
+eniesLobby.addEventListener("click", () => {
+  modalBg2.classList.add("activate-bg2");
+});
 
-buttonPencukur.addEventListener('mouseover', () => {
-    modalBg2.classList.add('activate-bg2')
-})
-
-modalClose2.addEventListener('mouseover', () => {
-    modalBg2.classList.remove('activate-bg2')
-})
+modalClose2.addEventListener("click", () => {
+  modalBg2.classList.remove("activate-bg2");
+});
